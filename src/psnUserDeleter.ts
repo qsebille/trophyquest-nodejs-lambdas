9430e5a53a8a0d9f198ac65502f4632e9a927b56
Quentin Sebille <quentin.sebille@gmail.com>
2025-11-24 16:21:44 +0100

    Added PSN user deleter script and associated database operations

---
+++ src/psnUserDeleter.ts
@@ -0,0 +1,33 @@
+import {getParams, Params} from "./config/params.js";
+import {buildPostgresPool} from "./postgres/utils/buildPostgresPool.js";
+import {Pool} from "pg";
+import {getPsnAuthTokens, PsnAuthTokens} from "./auth/psnAuthTokens.js";
+import {PsnUser} from "./psn/models/psnUser.js";
+import {fetchPsnUser} from "./psn/fetchPsnUser.js";
+import {deleteUserProfile} from "./postgres/deleteUserProfile.js";
+
+
+async function main() {
+    const startTime = Date.now();
+    console.info("START PSN Remover")
+
+    const params: Params = getParams();
+    const pool: Pool = buildPostgresPool();
+
+    try {
+        const psnAuthTokens: PsnAuthTokens = await getPsnAuthTokens(params.npsso);
+        const psnUser: PsnUser = await fetchPsnUser(psnAuthTokens, params.profileName);
+        const accountId: string = psnUser.id;
+        await deleteUserProfile(pool, accountId);
+        console.info("SUCCESS");
+    } finally {
+        const durationSeconds = (Date.now() - startTime) / 1000;
+        console.info(`Total processing time: ${durationSeconds.toFixed(2)} s`);
+        await pool.end();
+    }
+}
+
+main().catch((e) => {
+    console.error(e);
+    process.exitCode = 1;
+});
